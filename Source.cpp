a17224dc29c71a3574086d174e9110d4460408a6
IgnatievAnton <ignatievanton1996@gmail.com>
2017-10-12 16:29:47 +0400

    mydaw

--- Source.cpp
+++ Source.cpp
@@ -43,105 +43,6 @@ unsigned char range_limit(int x)
 GLOBAL(void)
 fdct(const unsigned char *input_buf, int input_stride, float *outptr) //const unsigned char * p, int stride, float *d)
 {
-	/*float tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
-	float tmp10, tmp11, tmp12, tmp13;
-	float z1, z2, z3, z4, z5, z11, z13;
-	float *dataptr;
-	int ctr;
-
-	dataptr = d;
-	for (ctr = 0; ctr < DCTSIZE; ctr++)
-	{
-		tmp0 = (float)(GETJSAMPLE(p[0]) + GETJSAMPLE(p[7]));
-		tmp7 = (float)(GETJSAMPLE(p[0]) - GETJSAMPLE(p[7]));
-		tmp1 = (float)(GETJSAMPLE(p[1]) + GETJSAMPLE(p[6]));
-		tmp6 = (float)(GETJSAMPLE(p[1]) - GETJSAMPLE(p[6]));
-		tmp2 = (float)(GETJSAMPLE(p[2]) + GETJSAMPLE(p[5]));
-		tmp5 = (float)(GETJSAMPLE(p[2]) - GETJSAMPLE(p[5]));
-		tmp3 = (float)(GETJSAMPLE(p[3]) + GETJSAMPLE(p[4]));
-		tmp4 = (float)(GETJSAMPLE(p[3]) - GETJSAMPLE(p[4]));
-
-		tmp10 = tmp0 + tmp3;
-		tmp13 = tmp0 - tmp3;
-		tmp11 = tmp1 + tmp2;
-		tmp12 = tmp1 - tmp2;
-
-		dataptr[0] = tmp10 + tmp11 - 8 * CENTERJSAMPLE;
-		dataptr[4] = tmp10 - tmp11;
-
-		z1 = (tmp12 + tmp13) * ((float) 0.707106781);
-		dataptr[2] = tmp13 + z1;
-		dataptr[6] = tmp13 - z1;
-
-		tmp10 = tmp4 + tmp5;
-		tmp11 = tmp5 + tmp6;
-		tmp12 = tmp6 + tmp7;
-
-		z5 = (tmp10 - tmp12) * ((float) 0.382683433);
-		z2 = ((float) 0.541196100) * tmp10 + z5;
-		z4 = ((float) 1.306562965) * tmp12 + z5;
-		z3 = tmp11 * ((float) 0.707106781);
-
-		z11 = tmp7 + z3;
-		z13 = tmp7 - z3;
-
-		dataptr[5] = z13 + z2;
-		dataptr[3] = z13 - z2;
-		dataptr[1] = z11 + z4;
-		dataptr[7] = z11 - z4;
-
-		dataptr += DCTSIZE;
-		p += stride;
-	}
-
-
-	dataptr = d;
-	for (ctr = DCTSIZE - 1; ctr >= 0; ctr--)
-	{
-		tmp0 = dataptr[DCTSIZE * 0] + dataptr[DCTSIZE * 7];
-		tmp7 = dataptr[DCTSIZE * 0] - dataptr[DCTSIZE * 7];
-		tmp1 = dataptr[DCTSIZE * 1] + dataptr[DCTSIZE * 6];
-		tmp6 = dataptr[DCTSIZE * 1] - dataptr[DCTSIZE * 6];
-		tmp2 = dataptr[DCTSIZE * 2] + dataptr[DCTSIZE * 5];
-		tmp5 = dataptr[DCTSIZE * 2] - dataptr[DCTSIZE * 5];
-		tmp3 = dataptr[DCTSIZE * 3] + dataptr[DCTSIZE * 4];
-		tmp4 = dataptr[DCTSIZE * 3] - dataptr[DCTSIZE * 4];
-
-
-		tmp10 = tmp0 + tmp3;
-		tmp13 = tmp0 - tmp3;
-		tmp11 = tmp1 + tmp2;
-		tmp12 = tmp1 - tmp2;
-
-		dataptr[DCTSIZE * 0] = tmp10 + tmp11;
-		dataptr[DCTSIZE * 4] = tmp10 - tmp11;
-
-		z1 = (tmp12 + tmp13) *  0.707106781;
-		dataptr[DCTSIZE * 2] = tmp13 + z1;
-		dataptr[DCTSIZE * 6] = tmp13 - z1;
-
-
-
-		tmp10 = tmp4 + tmp5;
-		tmp11 = tmp5 + tmp6;
-		tmp12 = tmp6 + tmp7;
-
-
-		z5 = (tmp10 - tmp12) * 0.382683433;
-		z2 = 0.541196100 * tmp10 + z5;
-		z4 = 1.306562965 * tmp12 + z5;
-		z3 = tmp11 * 0.707106781;
-
-		z11 = tmp7 + z3;
-		z13 = tmp7 - z3;
-
-		dataptr[DCTSIZE * 5] = z13 + z2;
-		dataptr[DCTSIZE * 3] = z13 - z2;
-		dataptr[DCTSIZE * 1] = z11 + z4;
-		dataptr[DCTSIZE * 7] = z11 - z4;
-
-		dataptr++;
-	}*/
 	float tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
 	float tmp10, tmp11, tmp12, tmp13;
 	float z1, z2, z3, z4, z5, z11, z13;
@@ -244,7 +145,8 @@ fdct(const unsigned char *input_buf, int input_stride, float *outptr) //const un
 GLOBAL(void)
 idct(const float *inptr, unsigned char *output_buf, int output_stride) //const float *d, unsigned char *res, int stride)
 {
-	/*float tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
+
+	float tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
 	float tmp10, tmp11, tmp12, tmp13;
 	float z5, z10, z11, z12, z13;
 	float * wsptr;
@@ -253,12 +155,12 @@ idct(const float *inptr, unsigned char *output_buf, int output_stride) //const f
 
 	wsptr = workspace;
 	for (ctr = DCTSIZE; ctr > 0; ctr--) {
-		if (d[DCTSIZE * 1] == 0 && d[DCTSIZE * 2] == 0 &&
-			d[DCTSIZE * 3] == 0 && d[DCTSIZE * 4] == 0 &&
-			d[DCTSIZE * 5] == 0 && d[DCTSIZE * 6] == 0 &&
-			d[DCTSIZE * 7] == 0)
+		if (inptr[DCTSIZE * 1] == 0 && inptr[DCTSIZE * 2] == 0 &&
+			inptr[DCTSIZE * 3] == 0 && inptr[DCTSIZE * 4] == 0 &&
+			inptr[DCTSIZE * 5] == 0 && inptr[DCTSIZE * 6] == 0 &&
+			inptr[DCTSIZE * 7] == 0)
 		{
-			float dcval = d[DCTSIZE * 0];
+			float dcval = inptr[DCTSIZE * 0];
 
 			wsptr[DCTSIZE * 0] = dcval;
 			wsptr[DCTSIZE * 1] = dcval;
@@ -269,31 +171,31 @@ idct(const float *inptr, unsigned char *output_buf, int output_stride) //const f
 			wsptr[DCTSIZE * 6] = dcval;
 			wsptr[DCTSIZE * 7] = dcval;
 
-			d++;
+			inptr++;
 			wsptr++;
 			continue;
 		}
 
-		tmp0 = d[DCTSIZE * 0];
-		tmp1 = d[DCTSIZE * 2];
-		tmp2 = d[DCTSIZE * 4];
-		tmp3 = d[DCTSIZE * 6];
+		tmp0 = inptr[DCTSIZE * 0];
+		tmp1 = inptr[DCTSIZE * 2];
+		tmp2 = inptr[DCTSIZE * 4];
+		tmp3 = inptr[DCTSIZE * 6];
 
 		tmp10 = tmp0 + tmp2;
 		tmp11 = tmp0 - tmp2;
 
 		tmp13 = tmp1 + tmp3;
-		tmp12 = (tmp1 - tmp3) *  1.414213562f - tmp13;
+		tmp12 = (tmp1 - tmp3) * 1.414213562f - tmp13;
 
 		tmp0 = tmp10 + tmp13;
 		tmp3 = tmp10 - tmp13;
 		tmp1 = tmp11 + tmp12;
 		tmp2 = tmp11 - tmp12;
 
-		tmp4 = d[DCTSIZE * 1];
-		tmp5 = d[DCTSIZE * 3];
-		tmp6 = d[DCTSIZE * 5];
-		tmp7 = d[DCTSIZE * 7];
+		tmp4 = inptr[DCTSIZE * 1];
+		tmp5 = inptr[DCTSIZE * 3];
+		tmp6 = inptr[DCTSIZE * 5];
+		tmp7 = inptr[DCTSIZE * 7];
 
 		z13 = tmp6 + tmp5;
 		z10 = tmp6 - tmp5;
@@ -304,12 +206,12 @@ idct(const float *inptr, unsigned char *output_buf, int output_stride) //const f
 		tmp11 = (z11 - z13) * 1.414213562f;
 
 		z5 = (z10 + z12) * 1.847759065f;
-		tmp10 = 1.082392200f * z12 - z5;
-		tmp12 = 2.613125930f * z10 + z5;
+		tmp10 = z5 - z12 * 1.082392200f;
+		tmp12 = z5 - z10 * 2.613125930f;
 
 		tmp6 = tmp12 - tmp7;
 		tmp5 = tmp11 - tmp6;
-		tmp4 = tmp10 + tmp5;
+		tmp4 = tmp10 - tmp5;
 
 		wsptr[DCTSIZE * 0] = tmp0 + tmp7;
 		wsptr[DCTSIZE * 7] = tmp0 - tmp7;
@@ -317,16 +219,18 @@ idct(const float *inptr, unsigned char *output_buf, int output_stride) //const f
 		wsptr[DCTSIZE * 6] = tmp1 - tmp6;
 		wsptr[DCTSIZE * 2] = tmp2 + tmp5;
 		wsptr[DCTSIZE * 5] = tmp2 - tmp5;
-		wsptr[DCTSIZE * 4] = tmp3 + tmp4;
-		wsptr[DCTSIZE * 3] = tmp3 - tmp4;
+		wsptr[DCTSIZE * 3] = tmp3 + tmp4;
+		wsptr[DCTSIZE * 4] = tmp3 - tmp4;
 
-		d++;
+		inptr++;
 		wsptr++;
 	}
 
+	/* Pass 2: process rows. */
+
 	wsptr = workspace;
-	for (ctr = 0; ctr < DCTSIZE; ctr++)
-	{
+	for (ctr = 0; ctr < DCTSIZE; ctr++) {
+		/* Prepare range-limit and float->int conversion */
 		z5 = wsptr[0] + (CENTERJSAMPLE + 0.5f);
 		tmp10 = z5 + wsptr[4];
 		tmp11 = z5 - wsptr[4];
@@ -339,7 +243,6 @@ idct(const float *inptr, unsigned char *output_buf, int output_stride) //const f
 		tmp1 = tmp11 + tmp12;
 		tmp2 = tmp11 - tmp12;
 
-
 		z13 = wsptr[5] + wsptr[3];
 		z10 = wsptr[5] - wsptr[3];
 		z11 = wsptr[1] + wsptr[7];
@@ -349,153 +252,26 @@ idct(const float *inptr, unsigned char *output_buf, int output_stride) //const f
 		tmp11 = (z11 - z13) * 1.414213562f;
 
 		z5 = (z10 + z12) * 1.847759065f;
-		tmp10 = z5 - 1.082392200f * z12;
-		tmp12 = z5 - 2.613125930f * z10;
+		tmp10 = z5 - z12 * 1.082392200f;
+		tmp12 = z5 - z10 * 2.613125930f;
 
 		tmp6 = tmp12 - tmp7;
 		tmp5 = tmp11 - tmp6;
-		tmp4 = tmp10 + tmp5;
+		tmp4 = tmp10 - tmp5;
 
-
-		res[0] = range_limit((int)(tmp0 + tmp7));
-		res[7] = range_limit((int)(tmp0 - tmp7));
-		res[1] = range_limit((int)(tmp1 + tmp6));
-		res[6] = range_limit((int)(tmp1 - tmp6));
-		res[2] = range_limit((int)(tmp2 + tmp5));
-		res[5] = range_limit((int)(tmp2 - tmp5));
-		res[4] = range_limit((int)(tmp3 + tmp4));
-		res[3] = range_limit((int)(tmp3 - tmp4));
+		/* Final output stage: float->int conversion and range-limit */
+		output_buf[0] = range_limit((int)(tmp0 + tmp7));
+		output_buf[7] = range_limit((int)(tmp0 - tmp7));
+		output_buf[1] = range_limit((int)(tmp1 + tmp6));
+		output_buf[6] = range_limit((int)(tmp1 - tmp6));
+		output_buf[2] = range_limit((int)(tmp2 + tmp5));
+		output_buf[5] = range_limit((int)(tmp2 - tmp5));
+		output_buf[3] = range_limit((int)(tmp3 + tmp4));
+		output_buf[4] = range_limit((int)(tmp3 - tmp4));
 
 		wsptr += DCTSIZE;
-		res += stride;
-	}*/
-
-float tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
-float tmp10, tmp11, tmp12, tmp13;
-float z5, z10, z11, z12, z13;
-float * wsptr;
-int ctr;
-float workspace[DCTSIZE2];
-
-wsptr = workspace;
-for (ctr = DCTSIZE; ctr > 0; ctr--) {
-	if (inptr[DCTSIZE * 1] == 0 && inptr[DCTSIZE * 2] == 0 &&
-		inptr[DCTSIZE * 3] == 0 && inptr[DCTSIZE * 4] == 0 &&
-		inptr[DCTSIZE * 5] == 0 && inptr[DCTSIZE * 6] == 0 &&
-		inptr[DCTSIZE * 7] == 0)
-	{
-		float dcval = inptr[DCTSIZE * 0];
-
-		wsptr[DCTSIZE * 0] = dcval;
-		wsptr[DCTSIZE * 1] = dcval;
-		wsptr[DCTSIZE * 2] = dcval;
-		wsptr[DCTSIZE * 3] = dcval;
-		wsptr[DCTSIZE * 4] = dcval;
-		wsptr[DCTSIZE * 5] = dcval;
-		wsptr[DCTSIZE * 6] = dcval;
-		wsptr[DCTSIZE * 7] = dcval;
-
-		inptr++;
-		wsptr++;
-		continue;
+		output_buf += output_stride;
 	}
-
-	tmp0 = inptr[DCTSIZE * 0];
-	tmp1 = inptr[DCTSIZE * 2];
-	tmp2 = inptr[DCTSIZE * 4];
-	tmp3 = inptr[DCTSIZE * 6];
-
-	tmp10 = tmp0 + tmp2;
-	tmp11 = tmp0 - tmp2;
-
-	tmp13 = tmp1 + tmp3;
-	tmp12 = (tmp1 - tmp3) * 1.414213562f - tmp13;
-
-	tmp0 = tmp10 + tmp13;
-	tmp3 = tmp10 - tmp13;
-	tmp1 = tmp11 + tmp12;
-	tmp2 = tmp11 - tmp12;
-
-	tmp4 = inptr[DCTSIZE * 1];
-	tmp5 = inptr[DCTSIZE * 3];
-	tmp6 = inptr[DCTSIZE * 5];
-	tmp7 = inptr[DCTSIZE * 7];
-
-	z13 = tmp6 + tmp5;
-	z10 = tmp6 - tmp5;
-	z11 = tmp4 + tmp7;
-	z12 = tmp4 - tmp7;
-
-	tmp7 = z11 + z13;
-	tmp11 = (z11 - z13) * 1.414213562f;
-
-	z5 = (z10 + z12) * 1.847759065f;
-	tmp10 = z5 - z12 * 1.082392200f;
-	tmp12 = z5 - z10 * 2.613125930f;
-
-	tmp6 = tmp12 - tmp7;
-	tmp5 = tmp11 - tmp6;
-	tmp4 = tmp10 - tmp5;
-
-	wsptr[DCTSIZE * 0] = tmp0 + tmp7;
-	wsptr[DCTSIZE * 7] = tmp0 - tmp7;
-	wsptr[DCTSIZE * 1] = tmp1 + tmp6;
-	wsptr[DCTSIZE * 6] = tmp1 - tmp6;
-	wsptr[DCTSIZE * 2] = tmp2 + tmp5;
-	wsptr[DCTSIZE * 5] = tmp2 - tmp5;
-	wsptr[DCTSIZE * 3] = tmp3 + tmp4;
-	wsptr[DCTSIZE * 4] = tmp3 - tmp4;
-
-	inptr++;
-	wsptr++;
-}
-
-/* Pass 2: process rows. */
-
-wsptr = workspace;
-for (ctr = 0; ctr < DCTSIZE; ctr++) {
-	/* Prepare range-limit and float->int conversion */
-	z5 = wsptr[0] + (CENTERJSAMPLE + 0.5f);
-	tmp10 = z5 + wsptr[4];
-	tmp11 = z5 - wsptr[4];
-
-	tmp13 = wsptr[2] + wsptr[6];
-	tmp12 = (wsptr[2] - wsptr[6]) * 1.414213562f - tmp13;
-
-	tmp0 = tmp10 + tmp13;
-	tmp3 = tmp10 - tmp13;
-	tmp1 = tmp11 + tmp12;
-	tmp2 = tmp11 - tmp12;
-
-	z13 = wsptr[5] + wsptr[3];
-	z10 = wsptr[5] - wsptr[3];
-	z11 = wsptr[1] + wsptr[7];
-	z12 = wsptr[1] - wsptr[7];
-
-	tmp7 = z11 + z13;
-	tmp11 = (z11 - z13) * 1.414213562f;
-
-	z5 = (z10 + z12) * 1.847759065f;
-	tmp10 = z5 - z12 * 1.082392200f;
-	tmp12 = z5 - z10 * 2.613125930f;
-
-	tmp6 = tmp12 - tmp7;
-	tmp5 = tmp11 - tmp6;
-	tmp4 = tmp10 - tmp5;
-
-	/* Final output stage: float->int conversion and range-limit */
-	output_buf[0] = range_limit((int)(tmp0 + tmp7));
-	output_buf[7] = range_limit((int)(tmp0 - tmp7));
-	output_buf[1] = range_limit((int)(tmp1 + tmp6));
-	output_buf[6] = range_limit((int)(tmp1 - tmp6));
-	output_buf[2] = range_limit((int)(tmp2 + tmp5));
-	output_buf[5] = range_limit((int)(tmp2 - tmp5));
-	output_buf[3] = range_limit((int)(tmp3 + tmp4));
-	output_buf[4] = range_limit((int)(tmp3 - tmp4));
-
-	wsptr += DCTSIZE;
-	output_buf += output_stride;
-}
 }
 
 GLOBAL(void)
@@ -567,7 +343,7 @@ read_JPEG_file(char * filename, int &width, int &height, unsigned short *quant_t
 	unsigned char *data = new unsigned char[cinfo.output_width*cinfo.output_height];
 	int row_count = 0;
 	if ((out_file = fopen("output.pgm", "wb")) == NULL)
-		printf("Файл невозможно открыть или создать\n");
+		printf("‘айл невозможно открыть или создать\n");
 	else {
 		fprintf(out_file, "P5\n%i %i\n255\n", cinfo.output_width, cinfo.output_height);
 		while (cinfo.output_scanline < cinfo.output_height) {
@@ -602,7 +378,7 @@ void write_file(const int width, const int height, unsigned char *data)
 {
 	FILE *out_file;
 	if ((out_file = fopen("res.pgm", "wb")) == NULL)
-		printf("Файл невозможно открыть или создать\n");
+		printf("‘айл невозможно открыть или создать\n");
 	else {
 		fprintf(out_file, "P5\n%i %i\n255\n", width, height);
 		fwrite(data, 1, width*height, out_file);
@@ -647,12 +423,6 @@ int main()
 	unsigned short quant_table[DCTSIZE2];
 	data = read_JPEG_file("testimg.jpg", width, height, quant_table);
 
-	
-	//for (int i = 0; i < DCTSIZE2; i++)
-	//{
-	//	quant_table[i] = 64;
-	//}
-
 	float ftable[DCTSIZE2], itable[DCTSIZE2];
 	jpeg_init_fdct_table(quant_table, ftable);
 	jpeg_init_idct_table(quant_table, itable);
@@ -675,49 +445,9 @@ int main()
 		}
 	}
 
-	//printf("result\n");
-	//for (int i = 0; i < 8; i++)
-	//{
-	//	for (int j = height - 9; j < height - 1; j++)
-	//	{
-	//		printf("%d ", result[i + j*width]);
-	//	}
-	//	printf("\n");
-	//}
-
-	//printf("summ\n");
-	//for (int i = 0; i < 8; i++)
-	//{
-	//	for (int j = height - 9; j < height - 1; j++)
-	//	{
-	//		printf("%i ", summ[i + j*width]);
-	//	}
-	//	printf("\n");
-	//}
 
 
 	divide(result, width, height, summ);
-
-	//printf("res after divide\n");
-	//for (int i = 0; i < 8; i++)
-	//{
-	//	for (int j = height - 9; j < height - 1; j++)
-	//	{
-	//		printf("%i ", result[i + j*width]);
-	//	}
-	//	printf("\n");
-	//}
-
-	//printf("data\n");
-	//for (int i = 0; i < 8; i++)
-	//{
-	//	for (int j = height - 9; j < height - 1; j++)
-	//	{
-	//		printf("%d ", data[i + j*width]);
-	//	}
-	//	printf("\n");
-	//}
-
 
 	write_file(width, height, summ);
 
