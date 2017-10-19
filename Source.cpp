d5d8c0f1a52760013e1c412e7256fc059135adb7
IgnatievAnton <ignatievanton1996@gmail.com>
2017-10-19 17:58:04 +0400

    DCT_poluOPENCL
    12

--- Source.cpp
+++ Source.cpp
@@ -6,13 +6,19 @@ extern "C"
 }
 #include <setjmp.h>
 #include <fstream>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <CL/cl.h>
+#include <stdbool.h>
 
-#define GETJSAMPLE(value)  ((int) (value))
-#define CENTERJSAMPLE	128
+
+
 
 #define DCTSIZE		    8
 #define DCTSIZE2	    64
-
 
 
 static const double aanscalefactor[DCTSIZE] =
@@ -40,242 +46,7 @@ unsigned char range_limit(int x)
 	return x < 0 ? 0 : x > 0xFF ? 0xFF : x;
 }
 
-GLOBAL(void)
-fdct(const unsigned char *input_buf, int input_stride, float *outptr) //const unsigned char * p, int stride, float *d)
-{
-	float tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
-	float tmp10, tmp11, tmp12, tmp13;
-	float z1, z2, z3, z4, z5, z11, z13;
-	float *dataptr;
-	int ctr;
-
-	/* Pass 1: process rows. */
-
-	dataptr = outptr;
-	for (ctr = 0; ctr < DCTSIZE; ctr++) {
-		tmp0 = (float)(GETJSAMPLE(input_buf[0]) + GETJSAMPLE(input_buf[7]));
-		tmp7 = (float)(GETJSAMPLE(input_buf[0]) - GETJSAMPLE(input_buf[7]));
-		tmp1 = (float)(GETJSAMPLE(input_buf[1]) + GETJSAMPLE(input_buf[6]));
-		tmp6 = (float)(GETJSAMPLE(input_buf[1]) - GETJSAMPLE(input_buf[6]));
-		tmp2 = (float)(GETJSAMPLE(input_buf[2]) + GETJSAMPLE(input_buf[5]));
-		tmp5 = (float)(GETJSAMPLE(input_buf[2]) - GETJSAMPLE(input_buf[5]));
-		tmp3 = (float)(GETJSAMPLE(input_buf[3]) + GETJSAMPLE(input_buf[4]));
-		tmp4 = (float)(GETJSAMPLE(input_buf[3]) - GETJSAMPLE(input_buf[4]));
-
-		tmp10 = tmp0 + tmp3;
-		tmp13 = tmp0 - tmp3;
-		tmp11 = tmp1 + tmp2;
-		tmp12 = tmp1 - tmp2;
-
-		/* Apply unsigned->signed conversion. */
-		dataptr[0] = tmp10 + tmp11 - 8 * CENTERJSAMPLE;
-		dataptr[4] = tmp10 - tmp11;
-
-		z1 = (tmp12 + tmp13) * 0.707106781f;
-		dataptr[2] = tmp13 + z1;
-		dataptr[6] = tmp13 - z1;
-
-		tmp10 = tmp4 + tmp5;
-		tmp11 = tmp5 + tmp6;
-		tmp12 = tmp6 + tmp7;
-
-		z5 = (tmp10 - tmp12) * 0.382683433f;
-		z2 = 0.541196100f * tmp10 + z5;
-		z4 = 1.306562965f * tmp12 + z5;
-		z3 = tmp11 * 0.707106781f;
-
-		z11 = tmp7 + z3;
-		z13 = tmp7 - z3;
-
-		dataptr[5] = z13 + z2;
-		dataptr[3] = z13 - z2;
-		dataptr[1] = z11 + z4;
-		dataptr[7] = z11 - z4;
-
-		dataptr += DCTSIZE;
-		input_buf += input_stride;
-	}
-
-	/* Pass 2: process columns. */
-
-	dataptr = outptr;
-	for (ctr = DCTSIZE - 1; ctr >= 0; ctr--) {
-		tmp0 = dataptr[DCTSIZE * 0] + dataptr[DCTSIZE * 7];
-		tmp7 = dataptr[DCTSIZE * 0] - dataptr[DCTSIZE * 7];
-		tmp1 = dataptr[DCTSIZE * 1] + dataptr[DCTSIZE * 6];
-		tmp6 = dataptr[DCTSIZE * 1] - dataptr[DCTSIZE * 6];
-		tmp2 = dataptr[DCTSIZE * 2] + dataptr[DCTSIZE * 5];
-		tmp5 = dataptr[DCTSIZE * 2] - dataptr[DCTSIZE * 5];
-		tmp3 = dataptr[DCTSIZE * 3] + dataptr[DCTSIZE * 4];
-		tmp4 = dataptr[DCTSIZE * 3] - dataptr[DCTSIZE * 4];
-
-		tmp10 = tmp0 + tmp3;
-		tmp13 = tmp0 - tmp3;
-		tmp11 = tmp1 + tmp2;
-		tmp12 = tmp1 - tmp2;
-
-		dataptr[DCTSIZE * 0] = tmp10 + tmp11;
-		dataptr[DCTSIZE * 4] = tmp10 - tmp11;
-
-		z1 = (tmp12 + tmp13) * 0.707106781f;
-		dataptr[DCTSIZE * 2] = tmp13 + z1;
-		dataptr[DCTSIZE * 6] = tmp13 - z1;
-
-		tmp10 = tmp4 + tmp5;
-		tmp11 = tmp5 + tmp6;
-		tmp12 = tmp6 + tmp7;
-
-		z5 = (tmp10 - tmp12) * 0.382683433f;
-		z2 = 0.541196100f * tmp10 + z5;
-		z4 = 1.306562965f * tmp12 + z5;
-		z3 = tmp11 * 0.707106781f;
-
-		z11 = tmp7 + z3;
-		z13 = tmp7 - z3;
-
-		dataptr[DCTSIZE * 5] = z13 + z2;
-		dataptr[DCTSIZE * 3] = z13 - z2;
-		dataptr[DCTSIZE * 1] = z11 + z4;
-		dataptr[DCTSIZE * 7] = z11 - z4;
-
-		dataptr++;
-	}
-}
-
-GLOBAL(void)
-idct(const float *inptr, unsigned char *output_buf, int output_stride) //const float *d, unsigned char *res, int stride)
-{
-
-	float tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
-	float tmp10, tmp11, tmp12, tmp13;
-	float z5, z10, z11, z12, z13;
-	float * wsptr;
-	int ctr;
-	float workspace[DCTSIZE2];
-
-	wsptr = workspace;
-	for (ctr = DCTSIZE; ctr > 0; ctr--) {
-		if (inptr[DCTSIZE * 1] == 0 && inptr[DCTSIZE * 2] == 0 &&
-			inptr[DCTSIZE * 3] == 0 && inptr[DCTSIZE * 4] == 0 &&
-			inptr[DCTSIZE * 5] == 0 && inptr[DCTSIZE * 6] == 0 &&
-			inptr[DCTSIZE * 7] == 0)
-		{
-			float dcval = inptr[DCTSIZE * 0];
-
-			wsptr[DCTSIZE * 0] = dcval;
-			wsptr[DCTSIZE * 1] = dcval;
-			wsptr[DCTSIZE * 2] = dcval;
-			wsptr[DCTSIZE * 3] = dcval;
-			wsptr[DCTSIZE * 4] = dcval;
-			wsptr[DCTSIZE * 5] = dcval;
-			wsptr[DCTSIZE * 6] = dcval;
-			wsptr[DCTSIZE * 7] = dcval;
-
-			inptr++;
-			wsptr++;
-			continue;
-		}
-
-		tmp0 = inptr[DCTSIZE * 0];
-		tmp1 = inptr[DCTSIZE * 2];
-		tmp2 = inptr[DCTSIZE * 4];
-		tmp3 = inptr[DCTSIZE * 6];
-
-		tmp10 = tmp0 + tmp2;
-		tmp11 = tmp0 - tmp2;
-
-		tmp13 = tmp1 + tmp3;
-		tmp12 = (tmp1 - tmp3) * 1.414213562f - tmp13;
-
-		tmp0 = tmp10 + tmp13;
-		tmp3 = tmp10 - tmp13;
-		tmp1 = tmp11 + tmp12;
-		tmp2 = tmp11 - tmp12;
-
-		tmp4 = inptr[DCTSIZE * 1];
-		tmp5 = inptr[DCTSIZE * 3];
-		tmp6 = inptr[DCTSIZE * 5];
-		tmp7 = inptr[DCTSIZE * 7];
-
-		z13 = tmp6 + tmp5;
-		z10 = tmp6 - tmp5;
-		z11 = tmp4 + tmp7;
-		z12 = tmp4 - tmp7;
-
-		tmp7 = z11 + z13;
-		tmp11 = (z11 - z13) * 1.414213562f;
-
-		z5 = (z10 + z12) * 1.847759065f;
-		tmp10 = z5 - z12 * 1.082392200f;
-		tmp12 = z5 - z10 * 2.613125930f;
-
-		tmp6 = tmp12 - tmp7;
-		tmp5 = tmp11 - tmp6;
-		tmp4 = tmp10 - tmp5;
-
-		wsptr[DCTSIZE * 0] = tmp0 + tmp7;
-		wsptr[DCTSIZE * 7] = tmp0 - tmp7;
-		wsptr[DCTSIZE * 1] = tmp1 + tmp6;
-		wsptr[DCTSIZE * 6] = tmp1 - tmp6;
-		wsptr[DCTSIZE * 2] = tmp2 + tmp5;
-		wsptr[DCTSIZE * 5] = tmp2 - tmp5;
-		wsptr[DCTSIZE * 3] = tmp3 + tmp4;
-		wsptr[DCTSIZE * 4] = tmp3 - tmp4;
-
-		inptr++;
-		wsptr++;
-	}
-
-	/* Pass 2: process rows. */
-
-	wsptr = workspace;
-	for (ctr = 0; ctr < DCTSIZE; ctr++) {
-		/* Prepare range-limit and float->int conversion */
-		z5 = wsptr[0] + (CENTERJSAMPLE + 0.5f);
-		tmp10 = z5 + wsptr[4];
-		tmp11 = z5 - wsptr[4];
-
-		tmp13 = wsptr[2] + wsptr[6];
-		tmp12 = (wsptr[2] - wsptr[6]) * 1.414213562f - tmp13;
-
-		tmp0 = tmp10 + tmp13;
-		tmp3 = tmp10 - tmp13;
-		tmp1 = tmp11 + tmp12;
-		tmp2 = tmp11 - tmp12;
-
-		z13 = wsptr[5] + wsptr[3];
-		z10 = wsptr[5] - wsptr[3];
-		z11 = wsptr[1] + wsptr[7];
-		z12 = wsptr[1] - wsptr[7];
-
-		tmp7 = z11 + z13;
-		tmp11 = (z11 - z13) * 1.414213562f;
-
-		z5 = (z10 + z12) * 1.847759065f;
-		tmp10 = z5 - z12 * 1.082392200f;
-		tmp12 = z5 - z10 * 2.613125930f;
-
-		tmp6 = tmp12 - tmp7;
-		tmp5 = tmp11 - tmp6;
-		tmp4 = tmp10 - tmp5;
-
-		/* Final output stage: float->int conversion and range-limit */
-		output_buf[0] = range_limit((int)(tmp0 + tmp7));
-		output_buf[7] = range_limit((int)(tmp0 - tmp7));
-		output_buf[1] = range_limit((int)(tmp1 + tmp6));
-		output_buf[6] = range_limit((int)(tmp1 - tmp6));
-		output_buf[2] = range_limit((int)(tmp2 + tmp5));
-		output_buf[5] = range_limit((int)(tmp2 - tmp5));
-		output_buf[3] = range_limit((int)(tmp3 + tmp4));
-		output_buf[4] = range_limit((int)(tmp3 - tmp4));
-
-		wsptr += DCTSIZE;
-		output_buf += output_stride;
-	}
-}
-
-GLOBAL(void)
-quant(float *d, float *ftable, float *itable)
+void quant(float *d, float *ftable, float *itable)
 {
 	for (int i = 0; i < DCTSIZE2; i++)
 	{
@@ -301,8 +72,7 @@ my_error_exit(j_common_ptr cinfo)
 	longjmp(myerr->setjmp_buffer, 1);
 }
 
-GLOBAL(unsigned char*)
-read_JPEG_file(char * filename, int &width, int &height, unsigned short *quant_table)
+unsigned char* read_JPEG_file(char * filename, int &width, int &height, unsigned short *quant_table)
 {
 	struct jpeg_decompress_struct cinfo;
 
@@ -386,35 +156,6 @@ void write_file(const int width, const int height, unsigned char *data)
 
 }
 
-void sum(short unsigned *result, unsigned char *summ, unsigned char *input, int stride)
-{
-	for (int x = 0; x < DCTSIZE; x++)
-	{
-		for (int y = 0; y < DCTSIZE; y++)
-		{
-			result[x + y*stride] += input[x + y*DCTSIZE];
-			summ[x + y*stride]++;
-		}
-	}
-}
-
-void divide(short unsigned *src, int width, int height, unsigned char *dst)
-{
-	/* if(x>=a && x<b) ==
-	if((unsigned int) (x-a) < (b-a))
-	*/
-
-	for (int j = 0; j < height; j++)
-	{
-		int ky = j < DCTSIZE ? j + 1 : j > height - DCTSIZE ? height - j : DCTSIZE;
-		for (int i = 0; i < width; i++)
-		{
-			int kx = i < DCTSIZE ? i + 1 : i > width - DCTSIZE ? width - i : DCTSIZE;
-			dst[i + j*width] = src[i + j*width] / (kx*ky);
-		}
-	}
-}
-
 int main()
 {
 	unsigned char *data;
@@ -422,37 +163,131 @@ int main()
 	int height;
 	unsigned short quant_table[DCTSIZE2];
 	data = read_JPEG_file("testimg.jpg", width, height, quant_table);
-
 	float ftable[DCTSIZE2], itable[DCTSIZE2];
 	jpeg_init_fdct_table(quant_table, ftable);
 	jpeg_init_idct_table(quant_table, itable);
-	short unsigned *result = new short unsigned[width * height];
-	memset(result, 0, width * height*sizeof(short));
+	//short unsigned *result = new short unsigned[width * height];
+	//memset(result, 0, width * height*sizeof(short));
 	unsigned char *summ = new unsigned char[width * height];
 	memset(summ, 0, width * height);
 	unsigned char output[DCTSIZE2];
 
 
-	for (int i = 0; i <= width - DCTSIZE; i++)
-	{
-		for (int j = 0; j <= height - DCTSIZE; j++)
-		{
-			float d[DCTSIZE2];
-			fdct(&data[i + j*width], width, d);
-			quant(d, ftable, itable);
-			idct(d, output, DCTSIZE);
-			sum(&result[i + j*width], &summ[i + j*width], output, width);
+	cl_uint num_platforms = 0;
+	clGetPlatformIDs(NULL, NULL, &num_platforms);
+	cl_platform_id *platforms = new cl_platform_id[num_platforms];
+	clGetPlatformIDs(num_platforms, platforms, NULL);
+	cl_device_type device_type = CL_DEVICE_TYPE_GPU;
+	cl_device_id *device = NULL;
+	for (int i = 0; i < num_platforms; i++) {
+		cl_uint num_devices = 0;
+		clGetDeviceIDs(platforms[i], device_type, 0, NULL, &num_devices);
+		if (num_devices >0) {
+			device = new cl_device_id[num_devices];
+			clGetDeviceIDs(platforms[i], device_type, num_devices, device, NULL);
+			break;
+		}
+	}
+	if (device == NULL) {
+		cl_device_type device_type = CL_DEVICE_TYPE_CPU;
+		for (int i = 0; i < num_platforms; i++) {
+			cl_uint num_devices = 0;
+			clGetDeviceIDs(platforms[i], device_type, 0, NULL, &num_devices);
+			if (num_devices >0) {
+				device = new cl_device_id[num_devices];
+				clGetDeviceIDs(platforms[i], device_type, num_devices, device, NULL);
+				break;
+			}
 		}
 	}
 
+	cl_uint buff;
+	clGetDeviceInfo(device[0], CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS, sizeof(cl_uint), &buff, NULL);
+	clGetDeviceInfo(device[0], CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(cl_uint), &buff, NULL);
+	cl_uint buff2[3];
+	clGetDeviceInfo(device[0], CL_DEVICE_MAX_WORK_ITEM_SIZES, sizeof(buff2), buff2, NULL);
+
+	cl_context context;
+	cl_int errContext;
+	context = clCreateContext(NULL, 1, &device[0], NULL, NULL, &errContext);
+
+	cl_command_queue com_queue;
+	cl_int errcodeQueue;
+	com_queue = clCreateCommandQueue(context, device[0], CL_QUEUE_PROFILING_ENABLE, &errcodeQueue);
+
+	FILE *pFile;
+	size_t  sFile;
+	char *buffer;
+	size_t result;
+	pFile = fopen("ker.cl", "rb");
+
+	const char *string = const_cast<char*>(buffer);
+
+	cl_program program = clCreateProgramWithSource(context, 1, &string, &sFile, NULL);
+	cl_int errProgramm;
+	errProgramm = clBuildProgram(program, 1, &device[0], " ", NULL, NULL);
+
+	char *buildLog = NULL;
+	clGetProgramBuildInfo(program, device[0], CL_PROGRAM_BUILD_LOG, 10240, buildLog, NULL);
+
+	const char *nameOfKernel = "ker";
+	int kernel_err;
+	cl_kernel kernel = clCreateKernel(program, nameOfKernel, &kernel_err);
+
+	int size1 = width*height;
+	int size2 = DCTSIZE2;
+
+	cl_mem sourceJPG = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(unsigned char)*size1, NULL, NULL);
+	cl_mem resultJPG = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(unsigned char)*size1, NULL, NULL);
+	cl_mem bufFtable = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(float)*size2, NULL, NULL);
+    cl_mem bufItable = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(float)*size2, NULL, NULL);
 
 
-	divide(result, width, height, summ);
+	int err_setParam;
+	err_setParam = clSetKernelArg(kernel, 0, sizeof(sourceJPG), &sourceJPG);
+	err_setParam = clSetKernelArg(kernel, 1, sizeof(resultJPG), &resultJPG);
+	err_setParam = clSetKernelArg(kernel, 2, sizeof(bufFtable), &bufFtable);
+	err_setParam = clSetKernelArg(kernel, 3, sizeof(bufItable), &bufItable);
+	err_setParam = clSetKernelArg(kernel, 4, sizeof(width), &width);
+	err_setParam = clSetKernelArg(kernel, 5, sizeof(height), &height);
+
+	int errWr;
+	int errWr1;
+	errWr = clEnqueueWriteBuffer(com_queue, sourceJPG, CL_FALSE, 0, sizeof(unsigned char)*size1, data, NULL, NULL, NULL);
+	errWr = clEnqueueWriteBuffer(com_queue, bufFtable, CL_FALSE, 0, sizeof(float)*size2, ftable, NULL, NULL, NULL);
+	errWr = clEnqueueWriteBuffer(com_queue, bufItable, CL_FALSE, 0, sizeof(float)*size2, itable, NULL, NULL, NULL);
+
+ 	const size_t global_work_size[] = {(width/ (DCTSIZE *2) )* DCTSIZE,(height / (DCTSIZE*2))*DCTSIZE};
+	const size_t local_work_size[] = {DCTSIZE, DCTSIZE};
+	cl_event event = 0;
+
+	errWr = clEnqueueNDRangeKernel(com_queue, kernel, 2, 0, global_work_size, local_work_size, NULL, NULL, &event);
+	errWr1 = clEnqueueReadBuffer(com_queue, resultJPG, CL_TRUE, 0, sizeof(unsigned char)*size1, summ, NULL, NULL, NULL);
+	//errWr = clEnqueueWriteBuffer(com_queue, resultJPG, CL_FALSE, 0, sizeof(int)*size1, b, NULL, NULL, NULL);
+
+	//d_C = clCreateBuffer(context, CL_MEM_READ_WRITE, mem_size_C, NULL, &err);
+	//d_A = clCreateBuffer(context, CL_MEM_READ_WRITE, mem_size_A, NULL, &err);
+	//d_B = clCreateBuffer(context, CL_MEM_READ_WRITE, mem_size_B, NULL, &err);
+
+	
+
+
+	//for (int i = 0; i <= width - DCTSIZE; i++)
+	//{
+	//	for (int j = 0; j <= height - DCTSIZE; j++)
+	//	{
+	//		float d[DCTSIZE2];
+	//		fdct(&data[i + j*width], width, d);
+	//		quant(d, ftable, itable);
+	//		idct(d, output, DCTSIZE);
+	//		sum(&result[i + j*width], &summ[i + j*width], output, width);
+	//	}
+	//}
+	//divide(result, width, height, summ);
 
 	write_file(width, height, summ);
 
 	delete data;
-	delete result;
 
 	return 0;
 }
