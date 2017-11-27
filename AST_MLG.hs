eb3575e0236967f01ffb4650392e8a49c4266f48
IgnatievAnton <ignatievanton1996@gmail.com>
2017-11-27 21:04:17 +0300

    Update AST_MLG.hs

--- AST_MLG.hs
+++ AST_MLG.hs
@@ -1,19 +1,30 @@
 -- MLG - my language good 
 -- This file it is AST for my language MLG
 
--- MLG - my language good -- This file it is AST for my language MLG
-data MLG.Program = MLG.Program ExprList deriving (Show, Eq)
-data MLG.ExprList = MLG.ExprList MLG.Expr MLG.ExprList 
-                    | MLG.ExprNull     deriving(Show, Eq)
-data MLG.Expr = MLG.Expr Srting MLG.Rval deriving (Show, Eq)
-data MLG.Opertor = Madd                
-                   | Msub
-                   | Mmul
-                   | Mdiv
-                   | Meq
-                   | Mxor deriving (Show, Eq)
-data MLG.Rval = Mint Int
-                | Mstring Srting
-                | MifMelse MLG.Rval MLG.Rval MLG.Rval
-                | Moper MLG.Rval MLG.Operator MLG.Rval
-                | Mfor MLG.Rval MLG.Operator MLG.Rval deriving(Show, Eq)
+-- x
+
+-- MLGProgram [MLGExprList (Mstring "x")]
+data MLGProgram = MLGProgram [MLGOperation] deriving (Show)
+
+data MLGOpertor = Madd                
+                | Msub 
+                | Mmul
+                | Mdiv
+                | Mxor deriving (Show, Eq)
+
+data MLGOperNotMat = Meq
+                   | MBolse
+                   | MMenshe 
+                   | MdoubleEq deriving (Show,Eq)
+
+data MLGRval = Mint Int
+             | Mstring String
+             | Moper MLGRval MLGOpertor MLGRval deriving (Show,Eq)
+             
+data MLGOperation = MifMelse MLGRval MLGOperNotMat MLGRval
+                  | Mfor MLGRval MLGOperNotMat MLGRval 
+                  | MfunckOpred String [String] [MLGOperation] 
+                  | MfunckColl String [MLGRval]
+                  | Return MLGRval  
+                  | Mopred String MLGRval deriving (Show, Eq)
+
